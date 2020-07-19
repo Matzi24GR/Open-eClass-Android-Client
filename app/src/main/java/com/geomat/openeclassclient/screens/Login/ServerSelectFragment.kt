9c4b8fbe18c2b06eb6b446f3ef75439af40c77e5
matzi24 <ggeomat@gmail.com>
2020-07-19 15:05:37 +0300

    Switched ServerSelectFragment.kt to ViewBinding

--- app/src/main/java/com/geomat/openeclassclient/screens/Login/ServerSelectFragment.kt
+++ app/src/main/java/com/geomat/openeclassclient/screens/Login/ServerSelectFragment.kt
@@ -1,4 +1,4 @@
-package com.geomat.openeclassclient
+package com.geomat.openeclassclient.screens.Login
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
@@ -7,31 +7,28 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.cardview.widget.CardView
 import androidx.constraintlayout.widget.ConstraintLayout
+import com.geomat.openeclassclient.R
+import com.geomat.openeclassclient.databinding.FragmentServerSelectBinding
 import com.google.android.material.bottomsheet.BottomSheetBehavior
 
 class ServerSelectFragment : Fragment() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-    }
+    private lateinit var binding: FragmentServerSelectBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_server_select, container, false)
+        binding = FragmentServerSelectBinding.inflate(inflater)
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        val serverListCard = view.findViewById<CardView>(R.id.ServerListCard)
+        val bottomSheetBehavior = BottomSheetBehavior.from(binding.bottomSheet)
 
-        val bottomSheetBehavior = BottomSheetBehavior.from(view.findViewById<ConstraintLayout>(R.id.bottom_sheet))
-
-        serverListCard.setOnClickListener {
+        binding.ServerListCard.setOnClickListener {
             bottomSheetBehavior.state = BottomSheetBehavior.STATE_HALF_EXPANDED
         }
     }
