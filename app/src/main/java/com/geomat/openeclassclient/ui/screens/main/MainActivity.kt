5b9679e10485a487876b092a31eb13cbe025e455
Matzi24GR <ggeomat@gmail.com>
2022-07-04 09:50:47 +0300

    Ellipsize toolbar labels

--- app/src/main/java/com/geomat/openeclassclient/ui/screens/main/MainActivity.kt
+++ app/src/main/java/com/geomat/openeclassclient/ui/screens/main/MainActivity.kt
@@ -14,6 +14,7 @@ import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
@@ -190,7 +191,7 @@ fun BottomNav(
                     }
                 },
                 icon = { Icon(destination.icon, stringResource(id = destination.label)) },
-                label = { Text(stringResource(id = destination.label)) }
+                label = { Text(stringResource(id = destination.label,), maxLines = 1, overflow = TextOverflow.Ellipsis) }
             )
         }
     }
