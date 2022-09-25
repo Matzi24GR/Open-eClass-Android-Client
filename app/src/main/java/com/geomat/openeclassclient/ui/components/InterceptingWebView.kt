1334c65986408a6fb7361ee861bbd7d63561fdd9
Matzi24GR <ggeomat@gmail.com>
2022-09-25 05:37:35 +0300

    Fix ExternalAuthScreen WebView

--- app/src/main/java/com/geomat/openeclassclient/ui/components/InterceptingWebView.kt
+++ app/src/main/java/com/geomat/openeclassclient/ui/components/InterceptingWebView.kt
@@ -1,10 +1,15 @@
 package com.geomat.openeclassclient.ui.components
 
+import android.annotation.SuppressLint
+import android.view.ViewGroup.LayoutParams.MATCH_PARENT
 import android.webkit.CookieManager
 import android.webkit.WebResourceRequest
 import android.webkit.WebView
 import android.webkit.WebViewClient
+import android.widget.LinearLayout
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.viewinterop.AndroidView
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.geomat.openeclassclient.ui.screens.NavGraphs
@@ -12,10 +17,13 @@ import com.geomat.openeclassclient.ui.screens.login.externalAuth.ExternalAuthVie
 import com.ramcosta.composedestinations.navigation.DestinationsNavigator
 import timber.log.Timber
 
+@SuppressLint("SetJavaScriptEnabled")
 @Composable
 fun CustomWebView(url: String, viewModel: ExternalAuthViewModel = hiltViewModel(), navigator: DestinationsNavigator) {
     AndroidView(factory = { context ->
         WebView(context).apply {
+            settings.javaScriptEnabled = true
+            layoutParams = LinearLayout.LayoutParams(MATCH_PARENT, MATCH_PARENT)
             val domain = url.replace("https://","").replace("http://","").replaceAfter("/","").replace("/","")
             webViewClient = TokenInterceptorWebViewClient(onSuccess = { url ->
                 Timber.i("Login Response: $url")
@@ -24,7 +32,7 @@ fun CustomWebView(url: String, viewModel: ExternalAuthViewModel = hiltViewModel(
             })
             loadUrl(url)
         }
-    })
+    }, modifier = Modifier.fillMaxSize())
 }
 
 class TokenInterceptorWebViewClient(val onSuccess: (url: String) -> Unit): WebViewClient() {
