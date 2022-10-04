3025f9e9bd506dbb53d61430f0160814090e80d4
Matzi24GR <ggeomat@gmail.com>
2022-10-04 04:19:17 +0300

    Refactor dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -52,9 +52,6 @@ android {
         }
     }
     namespace = "com.geomat.openeclassclient"
-    buildFeatures {
-        viewBinding = true
-    }
     applicationVariants.all {
         kotlin.sourceSets {
             getByName(name) {
@@ -65,100 +62,127 @@ android {
 }
 
 dependencies {
-    val moshi = "1.14.0"
-    val coroutines = "1.6.4"
-    val room = "2.4.3"
-    val retrofit = "2.9.0"
-    val work = "1.0.1"
-    val compose = "1.2.1"
-    val hilt = "2.44"
-    val destinations = "1.6.20-beta"
-    val glide = "2.0.0"
-    val xmlUtil = "0.84.3"
+    // UI
+    val accompanist     = "0.25.1"
+    val appCompat       = "1.5.1"
+    val compose         = "1.2.1"
+    val composeNav      = "1.0.0"
+    val destinations    = "1.6.20-beta"
+    val glide           = "2.0.0"
 
-    //Timber Logging Library
-    implementation("com.jakewharton.timber:timber:5.0.1")
+    // Network
+    val jsoup           = "1.15.3"
+    val moshi           = "1.14.0"
+    val retrofit        = "2.9.0"
+    val xmlUtil         = "0.84.3"
 
-    //Material
-    implementation("com.google.android.material:material:1.6.1")
+    // Logic
+    val coroutines      = "1.6.4"
+    val dataStore       = "1.0.0"
+    val hilt            = "2.44"
+    val hiltWork        = "1.0.0"
+    val room            = "2.4.3"
+    val timber          = "5.0.1"
+    val work            = "1.0.1"
 
-    //Retrofit
-    implementation("com.squareup.retrofit2:retrofit:$retrofit")
-    implementation("com.squareup.retrofit2:converter-scalars:$retrofit")
-    implementation("com.squareup.retrofit2:converter-moshi:$retrofit")
-    implementation("com.jakewharton.retrofit:retrofit2-kotlinx-serialization-converter:0.8.0")
 
-    // Moshi
-    implementation("com.squareup.moshi:moshi:$moshi")
-    implementation("com.squareup.moshi:moshi-kotlin:$moshi")
-    kapt("com.squareup.moshi:moshi-kotlin-codegen:$moshi")
 
-    // XmlUtil
-    implementation("io.github.pdvrieze.xmlutil:core-android:$xmlUtil")
-    implementation("io.github.pdvrieze.xmlutil:serialization-android:$xmlUtil")
+    /*
+    *   UI Dependencies
+    */
+
+    // App Compat
+    implementation("androidx.appcompat:appcompat:$appCompat")
+
+    // Jetpack Compose
+    implementation("androidx.compose.material:material:$compose")
+    implementation("androidx.compose.animation:animation:$compose")
+    implementation("androidx.compose.ui:ui-tooling:$compose")
+    implementation("androidx.compose.runtime:runtime-livedata:$compose")
+    implementation("androidx.compose.material:material-icons-extended:$compose")
+    androidTestImplementation ("androidx.compose.ui:ui-test-junit4:$compose")
+
+    // Navigation
+    implementation("androidx.hilt:hilt-navigation-compose:$composeNav")
+
+    // Compose Destinations
+    implementation("io.github.raamcosta.compose-destinations:animations-core:$destinations")
+    ksp("io.github.raamcosta.compose-destinations:ksp:$destinations")
+
+    // Accompanist
+    implementation("com.google.accompanist:accompanist-placeholder:$accompanist")
 
     // Glide
     implementation("com.github.skydoves:landscapist-glide:$glide")
     implementation("com.github.skydoves:landscapist-animation:$glide")
     implementation("com.github.skydoves:landscapist-placeholder:$glide")
 
-    // JSoup
-    implementation("org.jsoup:jsoup:1.15.3")
 
-    // Android KTX
-    implementation("androidx.core:core-ktx:1.9.0")
+
+    /*
+    *   Network Dependencies
+    */
+
+    // Retrofit
+    implementation("com.squareup.retrofit2:retrofit:$retrofit")
+    implementation("com.squareup.retrofit2:converter-scalars:$retrofit")
+    implementation("com.squareup.retrofit2:converter-moshi:$retrofit")
+    implementation("com.jakewharton.retrofit:retrofit2-kotlinx-serialization-converter:0.8.0")
+
+    // XmlUtil
+    implementation("io.github.pdvrieze.xmlutil:core-android:$xmlUtil")
+    implementation("io.github.pdvrieze.xmlutil:serialization-android:$xmlUtil")
+
+    // Moshi
+    implementation("com.squareup.moshi:moshi:$moshi")
+    implementation("com.squareup.moshi:moshi-kotlin:$moshi")
+    kapt("com.squareup.moshi:moshi-kotlin-codegen:$moshi")
+
+    // JSoup
+    implementation("org.jsoup:jsoup:$jsoup")
+
+
+
+    /*
+    *   Logic Dependencies
+    */
+
+    //Timber Logging Library
+    implementation("com.jakewharton.timber:timber:$timber")
 
     // Room and Lifecycle dependencies
     implementation("androidx.room:room-runtime:$room")
     kapt("androidx.room:room-compiler:$room")
     implementation ("androidx.room:room-ktx:$room")
-    implementation("androidx.lifecycle:lifecycle-extensions:2.2.0")
 
     // WorkManager
     implementation("android.arch.work:work-runtime-ktx:$work")
-    //   required to avoid crash on Android 12 API 31
     implementation("androidx.work:work-runtime-ktx:2.7.1")
-
 
     // Coroutines
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:$coroutines")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:$coroutines")
 
-    // Navigation
-    implementation("androidx.hilt:hilt-navigation-compose:1.0.0")
+    // Hilt
+    implementation("com.google.dagger:hilt-android:$hilt")
+    kapt("com.google.dagger:hilt-android-compiler:$hilt")
+    implementation("androidx.hilt:hilt-work:$hiltWork")
+    kapt("androidx.hilt:hilt-compiler:$hiltWork")
 
-    // Testing
+    // DataStore
+    implementation("androidx.datastore:datastore:$dataStore")
+    implementation("androidx.datastore:datastore-preferences:$dataStore")
+
+
+
+    /*
+    *   Test Dependencies
+    */
+
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.3")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.4.0")
 
-    // Hilt
-    implementation("com.google.dagger:hilt-android:$hilt")
-    kapt("com.google.dagger:hilt-android-compiler:$hilt")
-    implementation("androidx.hilt:hilt-work:1.0.0")
-    kapt("androidx.hilt:hilt-compiler:1.0.0")
-
-    // Jetpack Compose
-    implementation("androidx.activity:activity-compose:1.6.0")
-    implementation("androidx.compose.material:material:1.2.1")
-    implementation("androidx.compose.animation:animation:1.2.1")
-    implementation("androidx.compose.ui:ui-tooling:1.2.1")
-    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.5.1")
-    androidTestImplementation ("androidx.compose.ui:ui-test-junit4:1.2.1")
-    implementation("androidx.appcompat:appcompat:1.5.1")
-    implementation("androidx.compose.runtime:runtime-livedata:$compose")
-    implementation("androidx.compose.material:material-icons-extended:$compose")
-
-    // Compose Destinations
-    implementation("io.github.raamcosta.compose-destinations:animations-core:$destinations")
-    ksp("io.github.raamcosta.compose-destinations:ksp:$destinations")
-
-    // DataStore
-    implementation("androidx.datastore:datastore:1.0.0")
-    implementation("androidx.datastore:datastore-preferences:1.0.0")
-
-    // Accompanist
-    implementation("com.google.accompanist:accompanist-placeholder:0.25.1")
 
 }
 repositories {
